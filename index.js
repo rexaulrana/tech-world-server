4d292be015edbf33d6ba2b02c2fe25d2bc66b316
rexaulrana <rexaul.rana@gmail.com>
2023-11-27 13:26:24 +0600

    trending products data get and patched for upvote

--- index.js
+++ index.js
@@ -27,15 +27,21 @@ async function run() {
 
     await client.connect();
     const featuresCollection = client.db("techDB").collection("featured");
+    const trendingCollection = client.db("techDB").collection("trending");
 
     //   get features items
     app.get("/features", async (req, res) => {
+      const filter = req.query;
+      // const query = {};
+      // const options = {
+      //   sort: { release_date: filter.sort === "new" ? 1 : -1 },
+      // };
       const result = await featuresCollection.find().toArray();
       res.send(result);
-      //   console.log(result);
+      // console.log(result);
     });
 
-    // update upvote
+    // update upvote for featured product
     app.patch("/features/:id", async (req, res) => {
       const id = req.params.id;
       const latestVote = req.body.upVote;
@@ -47,9 +53,38 @@ async function run() {
         },
       };
       const result = await featuresCollection.updateOne(filter, updateDoc);
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
+
+    // get all trending products
+    app.get("/trending", async (req, res) => {
+      const filter = req.query;
+      const query = {};
+      const options = {
+        sort: { up_vote: filter.query === "largest" ? 1 : -1 },
+      };
+      const result = await trendingCollection.find(query, options).toArray();
+      res.send(result);
+      console.log(result);
+    });
+    // update upvote for trending product
+    app.patch("/trending/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const latestVote = req.body.upVote;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          up_vote: latestVote,
+        },
+      };
+      const result = await trendingCollection.updateOne(filter, updateDoc);
+      // console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
